d5f5ae2c9eaa2fd2e8c6dbcf87eb42d40b45d8c4
MashaShamina <95140399+MashaShamina@users.noreply.github.com>
2022-04-07 19:11:37 +0300

    Исправлены мелкие ошибки

--- tests.py
+++ tests.py
@@ -8,4 +8,4 @@ class Tests(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    news = orm.relation("questions", back_populates='test')
+    questions = orm.relation("Questions", back_populates='test')
