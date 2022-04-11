554b440ff6ff4d32ad76444d040c05ec164b9477
lnivnv <95042274+lnivnv@users.noreply.github.com>
2022-04-11 21:43:54 +0300

    Update app.py
    регистрация работает, авторизация еще нет

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-#from data import Articles
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from data import Articles
+import sqlite3
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 
-
 app = Flask(__name__)
+
+Tests = Articles()
+
 
 @app.route('/')
 def index():
@@ -17,7 +20,16 @@ def about():
     return render_template('about.html')
 
 
-# Register Form Class
+@app.route('/tests')
+def tests():
+    return render_template('tests.html', tests=Tests)
+
+
+@app.route('/test/<string:id>')
+def test(id):
+    return render_template('test.html', id=id)
+
+
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
@@ -29,7 +41,6 @@ class RegisterForm(Form):
     confirm = PasswordField('Confirm Password')
 
 
-# User Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
@@ -38,64 +49,48 @@ def register():
         email = form.email.data
         username = form.username.data
         password = sha256_crypt.encrypt(str(form.password.data))
-
-        # Create cursor
-        cur = mysql.connection.cursor()
-
-        # Execute query
-        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
-
-        # Commit to DB
-        mysql.connection.commit()
-
-        # Close connection
-        cur.close()
-
-        flash('You are now registered and can log in', 'success')
-
+        con = sqlite3.connect("users.db")
+        cur = con.cursor()
+        cur.execute("""INSERT INTO users(name, email, username, password) VALUES(?, ?, ?, ?, ?)""",
+                    (name, email, username, password, ))
+        con.commit()
+        con.close()
+        # flash('You are now registered and can log in', 'success')
         return redirect(url_for('login'))
+
     return render_template('register.html', form=form)
 
 
-# User login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Get Form Fields
         username = request.form['username']
         password_candidate = request.form['password']
-
-        # Create cursor
-        cur = mysql.connection.cursor()
-
-        # Get user by username
-        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
-
-        if result > 0:
+        con = sqlite3.connect("users.db")
+        cur = con.cursor()
+        result = cur.execute("""SELECT * FROM users WHERE username = ?""", (username, ))
+        print(len(list(result)))
+        if len(list(result)) == :
             # Get stored hash
             data = cur.fetchone()
+            print(data)
             password = data['password']
-
-            # Compare Passwords
             if sha256_crypt.verify(password_candidate, password):
-                # Passed
                 session['logged_in'] = True
                 session['username'] = username
-
-                flash('You are now logged in', 'success')
-                return redirect(url_for('dashboard'))
+                return 'You are now logged in'
             else:
                 error = 'Invalid login'
                 return render_template('login.html', error=error)
             # Close connection
-            cur.close()
+            con.close()
         else:
             error = 'Username not found'
             return render_template('login.html', error=error)
 
     return render_template('login.html')
 
-# Check if user logged in
+
 def is_logged_in(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -106,35 +101,13 @@ def is_logged_in(f):
             return redirect(url_for('login'))
     return wrap
 
-# Logout
+
 @app.route('/logout')
 @is_logged_in
 def logout():
     session.clear()
     flash('You are now logged out', 'success')
     return redirect(url_for('login'))
-
-# Dashboard
-@app.route('/dashboard')
-@is_logged_in
-def dashboard():
-    # Create cursor
-    cur = mysql.connection.cursor()
-
-    # Get articles
-    #result = cur.execute("SELECT * FROM articles")
-    # Show articles only from the user logged in 
-    result = cur.execute("SELECT * FROM articles WHERE author = %s", [session['username']])
-
-    articles = cur.fetchall()
-
-    if result > 0:
-        return render_template('dashboard.html', articles=articles)
-    else:
-        msg = 'No Articles Found'
-        return render_template('dashboard.html', msg=msg)
-    # Close connection
-    cur.close()
 
 
 if __name__ == '__main__':
