07ff5bd10c6fcef4a9e9aee0d28155b2cce10a5d
MashaShamina <95140399+MashaShamina@users.noreply.github.com>
2022-04-11 19:30:01 +0300

    Сделана третья таблица. БД готова

--- main_Masha.py
+++ main_Masha.py
@@ -2,6 +2,7 @@ from flask import Flask
 from data import db_session
 from data.tests import Tests
 from data.questions import Questions
+from data.answers import Answers
 import csv
 
 app = Flask(__name__)
@@ -40,6 +41,31 @@ def question():
             db_sess.commit()
 
 
+def answer():
+    db_session.global_init("db/tests.db")
+    with open('answers.csv', encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        period = 0
+        questionid = 1
+        for index, row in enumerate(reader):
+            db_sess = db_session.create_session()
+            if row[1] == 'False':
+                if period == 3:
+                    period = 0
+                    questionid += 1
+                if period < 3:
+                    answers = Answers(title=row[0], is_correct=False, question_id=questionid)
+                    db_sess.add(answers)
+                    db_sess.commit()
+                    period += 1
+            else:
+                answers = Answers(title=row[0], is_correct=True, question_id=questionid)
+                db_sess.add(answers)
+                db_sess.commit()
+
+
 if __name__ == '__main__':
     test()
     question()
+    answer()
+
