48c116755263e6daf44374ae289bea542afce87f
lnivnv <95042274+lnivnv@users.noreply.github.com>
2022-04-11 21:45:10 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    tests = [
+        {
+            'id': 1,
+            'title': 'Test One',
+            'body': 'harry potter',
+            'author': 'Masha Shamina',
+            'create_date': '11-04-2022'
+        },
+        {
+            'id': 2,
+            'title': 'Test Two',
+            'body': 'harry potter',
+            'author': 'Masha Shamina',
+            'create_date': '11-04-2022'
+        },
+        {
+            'id': 3,
+            'title': 'Test Three',
+            'body': 'harry potter',
+            'author': 'Masha Shamina',
+            'create_date': '11-04-2022'
+        }
+    ]
+    return tests
