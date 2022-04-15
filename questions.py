fc262e6c835cdd3272a521d6832a9fd40b9152e2
MashaShamina <95140399+MashaShamina@users.noreply.github.com>
2022-04-15 21:37:03 +0300

    Файл был убран из папки

--- questions.py
+++ questions.py
@@ -1,13 +1,14 @@
 import sqlalchemy
 from sqlalchemy import orm
-from .db_session import SqlAlchemyBase
+from db_session import SqlAlchemyBase
 
 
 class Questions(SqlAlchemyBase):
-    __tablename__ = 'questions'
+    __tablename__ = 'Questions'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     picture = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     test_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("Tests.id"))
     test = orm.relation('Tests')
+    answers = orm.relation("Answers", back_populates='question')
